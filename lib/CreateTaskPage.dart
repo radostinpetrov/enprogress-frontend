6e5bcbe7c47b8ef80cf3f3d70a4921f99876c61d
Vishwam <vkg18@ic.ac.uk>
2020-06-03 03:16:40 +0100

    [vkg18] created list to add progress measures

--- lib/CreateTaskPage.dart
+++ lib/CreateTaskPage.dart
@@ -8,6 +8,46 @@ class CreateTaskPage extends StatefulWidget {
 
 class _CreateTaskPageState extends State<CreateTaskPage> {
   var _dateTime;
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey();
+  List<String> data = [];
+  final nameController = TextEditingController();
+  final progressController = TextEditingController();
+
+  Widget _buildItem(String item, Animation<double> animation) {
+    return SizeTransition(
+      sizeFactor: animation,
+      child: ListTile(
+          title: Text(item,
+        style: TextStyle(fontSize: 15),
+      ),
+      trailing: IconButton(
+      icon: Icon(Icons.remove),
+        onPressed: () {
+          _removeItem(item);
+        }),
+
+      ),);
+  }
+
+void _removeItem(String item) {
+  int index = data.indexOf(item);
+  if (index >= 0) {
+    data.removeAt(index);
+    AnimatedListRemovedItemBuilder builder = (context, animation) {
+      return _buildItem(item, animation);
+    };
+    _listKey.currentState.removeItem(index, builder);
+  }
+}
+
+  void _insertItem() {
+    String item = progressController.text;
+    if (item.length > 0) {
+      data.insert(0, item);
+      _listKey.currentState.insertItem(0);
+      progressController.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,6 +90,7 @@ class _CreateTaskPageState extends State<CreateTaskPage> {
             ),
           ),
           TextField(
+            controller: nameController,
             textAlign: TextAlign.center,
             decoration: InputDecoration(
               border: UnderlineInputBorder(),
@@ -89,22 +130,59 @@ class _CreateTaskPageState extends State<CreateTaskPage> {
         ]));
 
     Widget subGoals = Container(
-      height: 100,
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          color: Colors.white70,
-          shape: BoxShape.rectangle,
-          borderRadius: BorderRadius.only(
-              topRight: Radius.circular(25.0),
-              bottomLeft: Radius.circular(25.0)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.red,
-              offset: Offset(-10, 10),
-              blurRadius: 20,
-            )
-          ]),
-    );
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.white70,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(25.0),
+                bottomLeft: Radius.circular(25.0)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.red,
+                offset: Offset(-10, 10),
+                blurRadius: 20,
+              )
+            ]),
+        child: Column(children: [
+          Text(
+            'Add Progress Measures',
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.black,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(
+                  child: TextField(
+                controller: progressController,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  hintText: 'Enter Here',
+                ),
+              )),
+              IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    _insertItem();
+                  })
+            ],
+          ),
+          Container(
+              constraints: BoxConstraints(
+                maxHeight: 200
+              ),
+              child: AnimatedList(
+                key: _listKey,
+                initialItemCount: 0,
+                itemBuilder: (context, index, animation) {
+                  return _buildItem(data[index], animation);
+                },
+              )),
+        ]));
 
     return Scaffold(
         resizeToAvoidBottomInset: false,
