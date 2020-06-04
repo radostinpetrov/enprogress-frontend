01d6da39329c92ee0745dbb38064eb53243e4961
Matyas Horkay <mh7618@ic.ac.uk>
2020-06-04 20:00:07 +0200

    [mh7618] Moved WorkMode to Pages folder, and added it to main page

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:drp29/page_widgets/ArchivePage.dart';
 import 'package:drp29/page_widgets/TasksPage.dart';
 import 'package:drp29/page_widgets/FriendsPage.dart';
 import 'package:flutter/material.dart';
-import 'work_mode_page.dart';
-import 'dart:math' as math;
+import 'page_widgets/WorkModePage.dart';
 import 'package:drp29/Globals.dart';
 
 void main() {
@@ -22,10 +21,11 @@ class MyApp extends StatelessWidget {
         controller: PageController(
           initialPage: 1,
         ),
-        children: <Widget>[
+        children: [
           FriendsPage(),
           TasksPage(),
-          ArchivePage()
+          ArchivePage(),
+          WorkModePage(),
         ],
       ),
     );
