7a1cee8050657d49fb5aa62e0f7082d1307f58e8
David Valaczaki <dmv18@ic.ac.uk>
2020-06-09 18:42:27 +0200

    added archive_page

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:drp29/page_widgets/TasksPage.dart';
 import 'package:drp29/page_widgets/friend_page_widgets/FriendsPage.dart';
 import 'package:drp29/user/User.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'page_widgets/WorkModePage.dart';
 import 'package:drp29/Globals.dart';
 
@@ -13,10 +14,19 @@ void main() {
 class MyApp extends StatelessWidget {
 
   final User user = new User(username: "test");
+  final Client client = new Client();
+  final uri = Uri.parse("http://146.169.40.203:3000/tasks");
 
-  // This widget is the root of your application.
+  Future<String> _getTasks() async {
+    Response response = await client.get(uri);
+    String jsonResponse = response.body;
+    return jsonResponse;
+  }
+
   @override
   Widget build(BuildContext context) {
+    Future<String> data = _getTasks();
+
     return MaterialApp(
       title: "EnProgress",
       theme: Globals.theme,
@@ -27,8 +37,8 @@ class MyApp extends StatelessWidget {
         ),
         children: [
           FriendsPage(),
-          TasksPage(),
-          ArchivePage(),
+          TasksPage(data: data,),
+          ArchivePage(data: data,),
           WorkModePage(),
         ],
       ),
