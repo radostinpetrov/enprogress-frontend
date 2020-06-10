5ad3c89df8892ed8167f9e3777b118225afee85a
Matyas Horkay <mh7618@ic.ac.uk>
2020-06-10 20:07:30 +0200

    [mh7618] Changed homepage to be Landing page for sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:drp29/push_notifications.dart';
-import 'package:drp29/user/User.dart';
 import 'package:flutter/material.dart';
-import 'package:drp29/Globals.dart';
-import 'package:drp29/page_widgets/SignInPage.dart';
 import 'top_level/MyApp.dart';
 
 
@@ -11,21 +8,6 @@ void main() {
   PushNotificationsManager pushNotificationsManager = PushNotificationsManager();
   pushNotificationsManager.init();
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-
-  final User user = new User(username: "test");
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "EnProgress",
-      theme: Globals.theme,
-      home: LandingPage(),
-    );
-  }
 }
 
 
--- lib/page_widgets/SignInPage.dart
+++ lib/page_widgets/SignInPage.dart
@@ -2,8 +2,11 @@ import 'package:drp29/page_widgets/ArchivePage.dart';
 import 'package:drp29/page_widgets/TasksPage.dart';
 import 'package:drp29/page_widgets/WorkModePage.dart';
 import 'package:drp29/page_widgets/friend_page_widgets/FriendsPage.dart';
+import 'package:drp29/top_level/Globals.dart';
+import 'package:drp29/widgets/FloatingButton.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 
 class LandingPage extends StatelessWidget {
@@ -180,9 +183,19 @@ class HomePageState extends State<HomePage> {
     }
   }
 
+  final uri = Uri.parse("http://146.169.40.203:3000/tasks");
+  final Client client = new Client();
+
+  Future<String> _getTasks() async {
+    Response response = await client.get(uri);
+    String jsonResponse = response.body;
+    return jsonResponse;
+  }
+
   @override
   Widget build(BuildContext context) {
     String username = widget._user.email.toString();
+    Future<String> data = _getTasks();
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
@@ -199,17 +212,37 @@ class HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: PageView(
-        allowImplicitScrolling: false,
-        controller: PageController(
-          initialPage: 1,
-        ),
-        children: [
-          FriendsPage(),
-          TasksPage(),
-          ArchivePage(),
-          WorkModePage(),
-        ],
+      backgroundColor: Globals.primaryBlue,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 50.0),
+        child: FloatingButton(),
+      ),
+      body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                flex: 100,
+                child: PageView(
+                  allowImplicitScrolling: false,
+                  controller: PageController(
+                    initialPage: 1,
+                  ),
+                  children: [
+                    FriendsPage(),
+                    TasksPage(data: data,),
+                    ArchivePage(data: data,),
+                    WorkModePage(),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Row(
+                  children: <Widget>[],
+                ),
+              ),
+            ],
+          )
       ),
     );
   }
